c9e21696d42d6d2b99f2a51d40fe1a483cb4a916
kmolumby <kelly.molumby@gmail.com>
2018-09-14 15:48:42 -0600

    fifth part

--- js/app.js
+++ js/app.js
@@ -65,3 +65,25 @@ const isItACow = () => {
 }
 
 console.log(isItACow('bird'));
+
+// E. Driver's Ed
+// Make a variable that will hold a person's age. Be semantic.
+
+// Write code that will print out "Here are the keys", if the age is 16 years or older.
+
+// If the age is younger than 16, a message should print "Sorry, you're too young."
+
+
+
+
+const giveKeys = (personsAge) => {
+    if (personsAge >= 16) {
+        console.log()
+        return 'Here are the keys';
+    } else {
+        return 'Sorry, you\'re too young';
+    }
+}
+
+
+console.log(giveKeys(56));
